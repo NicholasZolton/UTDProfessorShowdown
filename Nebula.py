f7f9603e9fc348eb0791c6e55a220cb689fa8aec
Ian Prigmore <61255674+IanPrigmore@users.noreply.github.com>
2022-04-08 22:29:49 -0500

    Update Nebula.py
    Removed method causing search to break due to Nebula key disabling

--- Nebula.py
+++ Nebula.py
@@ -21,13 +21,13 @@ def searchCourseProfessors(courseNum=2305, profNames=['Chin']):
 
 
 def getCourseNameWithNumber(coursePrefix='cs', courseNum=2305):
-	coursePrefix = coursePrefix.lower()
-	baseurl = 'http://api.utdnebula.com/'
-	header = {'Authorization': 'dd1h55UQUb8x5nQIPW2iJ1ABaIDx9iv7'}
-	queryurl = baseurl + '/v1/sections/search?course_number=' + str(courseNum)
-	response = requests.get(queryurl, headers=header)
+# 	coursePrefix = coursePrefix.lower()
+# 	baseurl = 'http://api.utdnebula.com/'
+# 	header = {'Authorization': 'dd1h55UQUb8x5nQIPW2iJ1ABaIDx9iv7'}
+# 	queryurl = baseurl + '/v1/sections/search?course_number=' + str(courseNum)
+# 	response = requests.get(queryurl, headers=header)
 
-	for i in response.json():
-		if i['course_prefix'] == coursePrefix:
-			return i['title']
+# 	for i in response.json():
+# 		if i['course_prefix'] == coursePrefix:
+# 			return i['title']
 	return 'No results for ' + coursePrefix.upper() + ' ' + str(courseNum)
